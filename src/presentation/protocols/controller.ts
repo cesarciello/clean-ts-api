b4e79a5a603110877d6f7d13d5c3a756f0e9bfd5
Cesar Ciello <Cesar Ciello>
2021-02-25 12:03:20 -0300

    chore: solved eslint request

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -1,4 +1,4 @@
-import { HttpResponse, HttpResquest } from "./http";
+import { HttpResponse, HttpResquest } from './http'
 
 export interface Controller {
   handle: (httpRequest: HttpResquest) => HttpResponse
