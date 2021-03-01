ffe8382d8a43b5c9d2a25740e60c8a438271ccf9
Cesar Ciello <Cesar Ciello>
2021-03-01 12:13:35 -0300

    refactor: test name to  cors is enable

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('Body Parser Middleware', () => {
-  test('should parser body as json', async () => {
+  test('should cors is enable', async () => {
     app.post('/test_cors', (req, res) => {
       res.send()
     })
