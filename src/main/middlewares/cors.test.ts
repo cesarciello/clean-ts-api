a37272af693fa60639dd0125b5ba94b90aec0dd9
Cesar Ciello <Cesar Ciello>
2021-03-01 12:19:46 -0300

    feat: ensure returns default content type as json

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('Body Parser Middleware', () => {
+describe('CORS Middleware', () => {
   test('should cors is enable', async () => {
     app.post('/test_cors', (req, res) => {
       res.send()
