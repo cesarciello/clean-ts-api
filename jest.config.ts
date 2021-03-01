0dd018055e7d631659a4c2391af4a0467295bd6a
Cesar Ciello <Cesar Ciello>
2021-03-01 10:25:20 -0300

    refactor: change  configs jest about mongodb

--- jest.config.ts
+++ jest.config.ts
@@ -93,7 +93,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: ['@shelf/jest-mongodb'],
+  preset: '@shelf/jest-mongodb',
 
   // Run tests from one or more projects
   // projects: undefined,
