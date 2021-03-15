20645d122e352d1910f586a4c77d18a17783625b
Cesar Ciello <cesarciello15@gmail.com>
2021-03-15 12:51:04 -0300

    test: ensure returns 200 on load survey route  success

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -4,9 +4,24 @@ import request from 'supertest'
 import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { AddSurveyModel } from '../../domain/usecases/add-survey'
 
 let surveyCollection: Collection
 let accountCollection: Collection
+
+const makeFakeSurveyData: AddSurveyModel = {
+  question: 'any_question',
+  answers: [
+    {
+      image: 'any_image',
+      answer: 'any_answer'
+    },
+    {
+      answer: 'other_answer'
+    }
+  ],
+  date: new Date()
+}
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -67,10 +82,28 @@ describe('Survey Routes', () => {
   })
 
   describe('GET /survey', () => {
-    test('should returns 403 on load survey without accessToken', async () => {
+    test('should returns 403 on load surveys without accessToken', async () => {
       await request(app)
         .get('/api/surveys')
         .expect(403)
+    })
+
+    test('should returns 200 on load surveys success', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Gabriel Maddox',
+        email: 'maddox.gab@gmail.com',
+        password: 'any_password',
+        role: 'admin'
+      })
+      const { ops: [{ _id }] } = res
+      const accessToken = sign({ id: _id }, env.jwtSecret)
+      await accountCollection.updateOne({ _id }, { $set: { accessToken } })
+      await surveyCollection.insertOne(makeFakeSurveyData)
+
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
     })
   })
 })
