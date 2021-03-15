f7a41e8543ea27c1765c13d9b6fd5aec29622d1e
Cesar Ciello <cesarciello15@gmail.com>
2021-03-15 11:14:17 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,5 +1,8 @@
+import { Hasher } from '../../protocols/criptograpy/hasher'
+import { AccountModel } from '../../../domain/models/account'
+import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
+import { AddAccountRepository } from '../../protocols/db/account/add-account-repository'
 import { LoadAccountByEmailRepository } from '../../protocols/db/account/load-account-by-email-repository'
-import { AccountModel, AddAccount, AddAccountModel, Hasher, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor(
