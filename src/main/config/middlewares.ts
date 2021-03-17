cbb61ba35716df4bc9ebcdeecc1fa219a328a740
Cesar Ciello <cesarciello15@gmail.com>
2021-03-17 15:09:50 -0300

    feat: add no cache middleware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser, contentType, cors } from '../middlewares'
+import { noCache } from '../middlewares/no-cache'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
   app.use(contentType)
+  app.use(noCache)
 }
