ed6e67d2c433c5c9550371a24f7072e9f94b8f7d
Cesar Ciello <cesarciello15@gmail.com>
2021-03-16 21:56:27 -0300

    test: ensure save survey result route returns 200 on save survey result success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,6 +1,58 @@
 import app from '@/main/config/app'
+import env from '@/main/config/env'
 import request from 'supertest'
+import { sign } from 'jsonwebtoken'
+import { Collection } from 'mongodb'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { SurveyModel } from '@/domain/models/survey'
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { AccountModel } from '@/domain/models/account'
+
+const makeFakeAccount = async (): Promise<AccountModel & { accessToken: string }> => {
+  const { ops: [account] } = await accountCollection.insertOne({
+    name: 'Gabriel Maddox',
+    email: 'maddox.gab@gmail.com',
+    password: 'any_password'
+  })
+  const accessToken = sign({ id: account._id }, env.jwtSecret)
+  await accountCollection.updateOne({ _id: account._id }, { $set: { accessToken } })
+  const parsedAccount = MongoHelper.map(account)
+  return {
+    ...parsedAccount,
+    accessToken
+  }
+}
+
+const makeFakeSurvey = async (): Promise<SurveyModel> => {
+  const { ops: [survey] } = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      },
+      {
+        answer: 'other_answer'
+      }
+    ],
+    date: new Date()
+  })
+  return MongoHelper.map(survey)
+}
+
+const makeFakeSurveyResult = async (surveyId: string, accountId: string, answer: string): Promise<SurveyResultModel> => {
+  const { ops: [surveyResult] } = await surveyResultCollection.insertOne({
+    surveyId,
+    accountId,
+    answer,
+    date: new Date()
+  })
+  return MongoHelper.map(surveyResult)
+}
+
+let surveyCollection: Collection
+let accountCollection: Collection
+let surveyResultCollection: Collection
 
 describe('Survey Result Routes', () => {
   beforeAll(async () => {
@@ -11,14 +63,37 @@ describe('Survey Result Routes', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('survey')
+    await surveyCollection.deleteMany({})
+    surveyResultCollection = await MongoHelper.getCollection('surveyResult')
+    await surveyResultCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('account')
+    await accountCollection.deleteMany({})
+  })
+
   describe('PUT /surveys/:surveyId/results', () => {
     test('should returns 403 on save survey result without accessToken', async () => {
       await request(app)
         .put('/api/surveys/any_id/results')
         .send({
-          answer: ''
+          answers: ''
         })
         .expect(403)
+    })
+
+    test('should returns 200 on save survey result success', async () => {
+      const account = await makeFakeAccount()
+      const survey = await makeFakeSurvey()
+      await makeFakeSurveyResult(survey.id, account.id, survey.answers[0].answer)
+
+      await request(app)
+        .put(`/api/surveys/${survey.id}/results`)
+        .set('x-access-token', account.accessToken)
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(200)
     })
   })
 })
