a06b6dca0c4e6c47d8c7610a4329812c407dee88
Cesar Ciello <cesarciello15@gmail.com>
2021-03-19 16:16:23 -0300

    docs: add load survey route

--- src/main/docs/paths/save-survey-result-path.ts
+++ src/main/docs/paths/save-survey-result-path.ts
@@ -38,5 +38,34 @@ export const saveSurveyResultPath = {
       500: serverErrorRequest,
       403: forbbidenRequest
     }
+  },
+  get: {
+    security: [{ ApiKeyAuth: [] }],
+    tags: ['Survey'],
+    summary: 'API load a result to a survey by your id',
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        schema: {
+          type: 'string'
+        },
+        required: true
+      }
+    ],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      500: serverErrorRequest,
+      403: forbbidenRequest
+    }
   }
 }
