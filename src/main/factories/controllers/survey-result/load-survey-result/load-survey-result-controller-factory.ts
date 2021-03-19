986751e0ca8048912e76b3b976644e5e33b0ceb9
Cesar Ciello <cesarciello15@gmail.com>
2021-03-19 15:52:00 -0300

    feat: ensure load survey route returns 403 on load survey result without accessToken

---
+++ src/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory.ts
@@ -0,0 +1,7 @@
+import { LoadSurveyResultController } from '@/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller'
+import { makeDbLoadSurveyById } from '@/main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory'
+import { makeDbLoadSurveyResult } from '@/main/factories/usecases/survey-result/load-survey-result/db-load-survey-result-factory'
+
+export const makeLoadSurveyResultController = (): LoadSurveyResultController => {
+  return new LoadSurveyResultController(makeDbLoadSurveyById(), makeDbLoadSurveyResult())
+}
--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -2,7 +2,9 @@ import { Router } from 'express'
 import { auth } from '@/main/middlewares/auth'
 import { adaptRoute } from '@/main/adapter/express/express-route-adapter'
 import { makeSaveSurveyResultController } from '../factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+import { makeLoadSurveyResultController } from '../factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory'
 
 export default (router: Router): void => {
   router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+  router.get('/surveys/:surveyId/results', auth, adaptRoute(makeLoadSurveyResultController()))
 }
