d77d283b50e92ab2f8436c427481a59b3be61c19
Cesar Ciello <cesarciello15@gmail.com>
2021-03-23 20:43:58 -0300

    feat: singUp resolver use singUp controller

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,8 +1,11 @@
 import { adaptResover } from '@/main/adapter/apollo'
-import { makeLoginController } from '@/main/factories/controllers/login'
+import { makeLoginController, makeSingupController } from '@/main/factories/controllers/login'
 
 export default {
   Query: {
     login: async (parent: any, args: any) => adaptResover(makeLoginController(), args)
+  },
+  Mutation: {
+    singUp: async (parent: any, args: any) => adaptResover(makeSingupController(), args)
   }
 }
