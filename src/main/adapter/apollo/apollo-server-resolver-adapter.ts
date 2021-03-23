ddc82be5da55808f56653554269b84cb56e89fcf
Cesar Ciello <cesarciello15@gmail.com>
2021-03-23 20:36:48 -0300

    feat: add statuscode error on exeptions

--- src/main/adapter/apollo/apollo-server-resolver-adapter.ts
+++ src/main/adapter/apollo/apollo-server-resolver-adapter.ts
@@ -1,6 +1,28 @@
-import { Controller } from '@/presentation/protocols'
+import { Controller, HttpResponse } from '@/presentation/protocols'
+import { UserInputError, AuthenticationError, ForbiddenError, ApolloError } from 'apollo-server-express'
+
+const httpResponseCases = {
+  200: (response: HttpResponse) => {
+    return response.body
+  },
+  204: (response: HttpResponse) => {
+    return response.body
+  },
+  400: (response: HttpResponse) => {
+    throw new UserInputError(response.body.message)
+  },
+  401: (response: HttpResponse) => {
+    throw new AuthenticationError(response.body.message)
+  },
+  403: (response: HttpResponse) => {
+    throw new ForbiddenError(response.body.message)
+  },
+  500: (response: HttpResponse) => {
+    throw new ApolloError(response.body.message)
+  }
+}
 
 export const adaptResover = async (controller: Controller, args: any): Promise<any> => {
   const httpResponse = await controller.handle(args)
-  return httpResponse.body
+  return httpResponseCases[httpResponse.statusCode](httpResponse)
 }
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -2,11 +2,38 @@ import { Express } from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import resolvers from '@/main/graphql/resolvers'
 import typeDefs from '@/main/graphql/type-defs'
+import { GraphQLError } from 'graphql'
+
+const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+    if (checkError(error, 'AuthenticationError')) {
+      response.http.status = 401
+    } else if (checkError(error, 'ForbiddenError')) {
+      response.http.status = 403
+    } else if (checkError(error, 'ApolloError')) {
+      response.http.status = 500
+    } else if (checkError(error, 'UserInputError')) {
+      response.http.status = 400
+    } else {
+      response.http.status = 500
+    }
+  })
+}
+
+const checkError = (error: GraphQLError, errorName: string): boolean => {
+  return [error.name, error.originalError?.name].some(name => name === errorName)
+}
 
 export default (app: Express): void => {
   const server = new ApolloServer({
     resolvers,
-    typeDefs
+    typeDefs,
+    plugins: [{
+      requestDidStart: () => ({
+        willSendResponse: ({ response, errors }) => handleErrors(response, errors)
+      })
+    }]
   })
   server.applyMiddleware({ app })
 }
