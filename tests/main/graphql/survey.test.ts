44d017de852957c9e0983f6625c1c1a1808f16c7
Cesar Ciello <cesarciello15@gmail.com>
2021-03-24 12:15:51 -0300

    test: ensure Survey Query return an Surveys on success

---
+++ tests/main/graphql/survey.test.ts
@@ -0,0 +1,95 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '@/infra/db/mongodb/helpers'
+import { ApolloServer, gql } from 'apollo-server-express'
+import { makeApolloServer } from './helpers'
+import { createTestClient } from 'apollo-server-integration-testing'
+import { sign } from 'jsonwebtoken'
+import env from '@/main/config/env'
+import { mockAddSurveyParams } from '@/tests/domain/mock'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+let apolloServer: ApolloServer
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'Gabriel Maddox',
+    email: 'maddox.gab@gmail.com',
+    password: 'any_password',
+    role: 'admin'
+  })
+  const { ops: [{ _id }] } = res
+  const accessToken = sign({ id: _id }, env.jwtSecret)
+  await accountCollection.updateOne({ _id }, { $set: { accessToken } })
+  return accessToken
+}
+
+describe('Survey GraphQL', () => {
+  beforeAll(async () => {
+    apolloServer = makeApolloServer()
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('survey')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('account')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('Surveys Query', () => {
+    const surveysQuery = gql`
+      query surveys {
+        surveys {
+          id,
+          question,
+          answers {
+            image,
+            answer
+          },
+          date,
+          didAnswer
+        }
+      }
+    `
+    test('should return an Surveys', async () => {
+      const accessToken = await makeAccessToken()
+      await surveyCollection.insertOne(mockAddSurveyParams())
+
+      const { query } = createTestClient({
+        apolloServer,
+        extendMockRequest: {
+          headers: {
+            'x-access-token': accessToken
+          }
+        }
+      })
+      const res: any = await query(surveysQuery)
+      console.log(JSON.stringify(res))
+      expect(res.data.surveys.length).toBe(1)
+      expect(res.data.surveys[0].id).toBeTruthy()
+      expect(res.data.surveys[0].question).toBe('any_question')
+      expect(res.data.surveys[0].didAnswer).toBe(false)
+      expect(res.data.surveys[0].answers).toEqual([{
+        image: 'any_image',
+        answer: 'any_answer'
+      }])
+    })
+
+    // test('should return an UnauthorizesError on invalid credentials', async () => {
+    //   const { query } = createTestClient({ apolloServer })
+    //   const res: any = await query(loginQuery, {
+    //     variables: {
+    //       email: 'maddox.gab@gmail.com',
+    //       password: '123'
+    //     }
+    //   })
+    //   expect(res.data).toBeFalsy()
+    //   expect(res.errors[0].message).toBe('Unauthorized')
+    // })
+  })
+})
