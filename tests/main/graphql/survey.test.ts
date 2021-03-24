434a0ab6c2d758af85fead46684a04969d41b213
Cesar Ciello <cesarciello15@gmail.com>
2021-03-24 12:18:03 -0300

    test: ensure Survey Query return an AccessDeniedError on invalid credentials

--- tests/main/graphql/survey.test.ts
+++ tests/main/graphql/survey.test.ts
@@ -80,16 +80,12 @@ describe('Survey GraphQL', () => {
       }])
     })
 
-    // test('should return an UnauthorizesError on invalid credentials', async () => {
-    //   const { query } = createTestClient({ apolloServer })
-    //   const res: any = await query(loginQuery, {
-    //     variables: {
-    //       email: 'maddox.gab@gmail.com',
-    //       password: '123'
-    //     }
-    //   })
-    //   expect(res.data).toBeFalsy()
-    //   expect(res.errors[0].message).toBe('Unauthorized')
-    // })
+    test('should return an AccessDeniedError on invalid credentials', async () => {
+      await surveyCollection.insertOne(mockAddSurveyParams())
+      const { query } = createTestClient({ apolloServer })
+      const res: any = await query(surveysQuery)
+      expect(res.data).toBeFalsy()
+      expect(res.errors[0].message).toBe('Access Denied')
+    })
   })
 })
